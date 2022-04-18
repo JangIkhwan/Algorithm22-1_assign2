407f74f642ad8d88321b8872db58afd5c89f42ac
JangIkhwan <87964632+JangIkhwan@users.noreply.github.com>
2022-04-18 21:11:05 +0900

    Add files via upload

--- quicksort.c
+++ quicksort.c
@@ -1,7 +1,7 @@
 void quicksort(int low, int high, int* list);
 void partition(int low, int high, int* pivot, int* list);
 
-// 퀵정렬을 수행하는 함수
+// 퀵 정렬을 수행하는 함수
 void quicksort(int low, int high, int* list) {
 	int pivot;
 	if (high > low) {
